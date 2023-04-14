0a7c4c3c65c25abbc10a8322b7077956ae68e5f0
5yxcanwin <1163825462@qq.com>
2023-04-15 00:43:37 +0800

    飞书、企业微信接入ChatGPT

--- src/main/java/com/example/mychat/common/BaseConstants.java
+++ src/main/java/com/example/mychat/common/BaseConstants.java
@@ -2,7 +2,7 @@ package com.example.mychat.common;
 
 public class BaseConstants {
 
-    public static final String api_key = "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";
+    public static final String api_key = "";
 
     public static final String model = "gpt-3.5-turbo";
     public static final Integer max_tokens = 2048;
@@ -11,22 +11,22 @@ public class BaseConstants {
     /**
      * 企业微信相关
      */
-    public static final String WxToken = "mDb5RqEhu2ECSiyMyP1QRc8uSvcEuR";
-    public static final String WxCorpID = "ww19fba3fa680503d8";
-    public static final String WxEncodingAESKey = "2I4rJNfVtrQBGEU4biSOfA3Fs3ySBpx0IbtHWx5KVYG";
-    public static final String WxCorpSecret = "2sSUj2frZNoQ4Mfp6s2H4sf-RF-egHLxRfH_K9kWo6c";
-    public static final Integer WxAgentID = 1000002;
+    public static final String WxToken = "";
+    public static final String WxCorpID = "";
+    public static final String WxEncodingAESKey = "";
+    public static final String WxCorpSecret = "";
+    public static final Integer WxAgentID = null;
 
     /**
      * 飞书相关
      */
-    public static final String FsAppID = "cli_a4b9675efc3d900d";
+    public static final String FsAppID = "";
 
-    public static final String FsAppSecret = "AlINg5wsVGfyUdNbOEWeedV35op1DJ8z";
+    public static final String FsAppSecret = "";
 
-    public static final String FsEncryptKey = "epjKVQbSMyN5vzvGHAcAE6vEEPZsUjaU";
+    public static final String FsEncryptKey = "";
 
-    public static final String FsVerificationToken = "TkAPxBfGCT1fIGitJBRtqjk8aNEHangg";
+    public static final String FsVerificationToken = "";
 
 
 }
